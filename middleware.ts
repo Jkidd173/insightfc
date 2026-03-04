121f127646645e3c82f456c871d0dae76fdf3b11
Josh Kidder <joshkidder@Joshs-MacBook-Pro-2.local>
2026-03-03 22:48:07 -0500

    fix middleware build error

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,12 @@
-import { NextResponse, type NextRequest } from "next/server";
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export async function middleware(req: NextRequest) {
-  const res = NextResponse.next();
-
-  // This keeps the Supabase session cookies fresh on every request
-  const supabase = createMiddlewareClient({ req, res });
-  await supabase.auth.getSession();
-
-  return res;
+export function middleware(req: NextRequest) {
+  return NextResponse.next();
 }
 
 export const config = {
   matcher: [
-    /*
-      Run middleware on all routes except:
-      - static files
-      - images
-      - favicon
-    */
     "/((?!_next/static|_next/image|favicon.ico).*)",
   ],
 };
